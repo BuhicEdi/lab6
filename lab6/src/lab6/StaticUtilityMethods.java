183ff3a99bf0bab8d8c3060bbbf8ac2840cbf9de
Edi Buhic <24edbu01@hig.se>
2024-10-07 11:25:27 +0200

    Lab 6

--- lab6/src/lab6/StaticUtilityMethods.java
+++ lab6/src/lab6/StaticUtilityMethods.java
@@ -40,15 +40,11 @@ public class StaticUtilityMethods {
 		return sum;
 	}
 
-	public static int calculateSum(int[] arrA, int[] arrB) {
+	public static int calculateSum(int[][] arr) {
 		int sum = 0;
 
-		for (int i = 0; i < arrA.length; i++) {
-			sum += arrA[i];
-		}
-
-		for (int i = 0; i < arrB.length; i++) {
-			sum += arrB[i];
+		for (int i = 0; i < arr.length; i++) {
+			sum += arr[i][i];
 		}
 		return sum;
 	}

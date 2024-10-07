0e9b421361fd0d747b70eb44cbbb85072617650f
Edi Buhic <24edbu01@hig.se>
2024-10-07 12:01:23 +0200

    Lab 6

--- lab6/src/lab6/P6_2.java
+++ lab6/src/lab6/P6_2.java
@@ -1,9 +1,16 @@
 package lab6;
 
+
 public class P6_2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		
+		int[][] arr = {{1, 2, 3}, {3, 2, 1}, {2, 1, 3}};
+		
+	//	System.out.println(arr[0][2]);
+	//	System.out.println(arr[1].length-1);
+		System.out.println(StaticUtilityMethods.calculateSum(arr));
 
 	}
 

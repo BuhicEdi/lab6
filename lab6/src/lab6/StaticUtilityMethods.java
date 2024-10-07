3b7e3bd16cb4da38d1d31621a8bdef12e4f15cb6
Edi Buhic <24edbu01@hig.se>
2024-10-07 11:07:45 +0200

    Lab 6

--- lab6/src/lab6/StaticUtilityMethods.java
+++ lab6/src/lab6/StaticUtilityMethods.java
@@ -10,15 +10,15 @@ public class StaticUtilityMethods {
 		return a + b;
 	}
 	
-	public double calculateSum(double a, double b) {
+	public static double calculateSum(double a, double b) {
 		return a + b;
 	}
 	
-	public int calculateSum(int a, int b, int c) {
+	public static int calculateSum(int a, int b, int c) {
 		return a + b + c;
 	}
 	
-	public double calculateSum(double a, double b, double c) {
+	public static double calculateSum(double a, double b, double c) {
 		return a + b + c;
 	}
 }
